e0f8b2df3af582b058dc7a0c74f08066bae71df3
nshumakov <mastershumakov@gmail.com>
2017-07-11 08:59:42 +0300

    All replaced

---
+++ app/src/main/java/nshumakov/com/spacetravel/GamePlay/GameView.java
@@ -0,0 +1,363 @@
+package nshumakov.com.spacetravel.GamePlay;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.media.MediaPlayer;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import nshumakov.com.spacetravel.Activities.MainActivity;
+import nshumakov.com.spacetravel.Models.Boss;
+import nshumakov.com.spacetravel.Models.Bullet;
+import nshumakov.com.spacetravel.Models.Enemy;
+import nshumakov.com.spacetravel.Models.Land;
+import nshumakov.com.spacetravel.Models.Player;
+import nshumakov.com.spacetravel.R;
+
+/**
+ * Created by nshumakov on 04.04.2017.
+ */
+
+public class GameView extends SurfaceView implements Runnable {
+
+    SurfaceHolder holder;
+    boolean ok = false;
+
+    private int[] tempImgsArray = new int[]{R.drawable.a_enemy_a, R.drawable.a_enemy_b, R.drawable.a_enemy_c, R.drawable.a_enemy_d, R.drawable.a_enemy_e};
+    private int[] imgsArrayLvl_b = new int[]{R.drawable.b_enemy_a, R.drawable.b_enemy_b, R.drawable.b_enemy_c, R.drawable.b_enemy_d, R.drawable.b_enemy_e};
+    private int[] imgsArrayLvl_c = new int[]{R.drawable.c_enemy_a, R.drawable.c_enemy_b, R.drawable.c_enemy_c, R.drawable.c_enemy_d, R.drawable.c_enemy_e};
+    private int[] imgsArrayLvl_d = new int[]{R.drawable.d_enemy_a, R.drawable.d_enemy_b, R.drawable.d_enemy_c, R.drawable.d_enemy_d, R.drawable.d_enemy_e};
+    private int[] imgsArrayLvl_e = new int[]{R.drawable.e_enemy_a, R.drawable.e_enemy_b, R.drawable.e_enemy_c, R.drawable.e_enemy_d, R.drawable.e_enemy_e};
+    private int[] imgsArrayLvl_f = new int[]{R.drawable.f_enemy_a, R.drawable.f_enemy_b, R.drawable.f_enemy_c, R.drawable.f_enemy_d, R.drawable.f_enemy_e};
+    private int[] imgsArrayLvl_g = new int[]{R.drawable.g_enemy_a, R.drawable.g_enemy_b, R.drawable.g_enemy_c, R.drawable.g_enemy_d, R.drawable.g_enemy_e};
+    private int[] imgsArrayLvl_h = new int[]{R.drawable.h_enemy_a, R.drawable.h_enemy_b, R.drawable.h_enemy_c, R.drawable.h_enemy_d, R.drawable.h_enemy_e};
+    private int[] imgsArrayLvl_i = new int[]{R.drawable.i_enemy_a, R.drawable.i_enemy_b, R.drawable.i_enemy_c, R.drawable.i_enemy_d, R.drawable.i_enemy_e};
+    private int[] imgsArrayLvl_j = new int[]{R.drawable.j_enemy_a, R.drawable.j_enemy_b, R.drawable.j_enemy_c, R.drawable.j_enemy_d, R.drawable.j_enemy_e};
+    private int[] imgsArrayLvl_k = new int[]{R.drawable.k_enemy_a, R.drawable.k_enemy_b, R.drawable.k_enemy_c, R.drawable.k_enemy_d, R.drawable.k_enemy_e};
+    private List<int[]> LvlPackArray = new ArrayList<>();
+    private int[] landscapes = new int[]{R.drawable.landscape_a, R.drawable.landscape_b, R.drawable.landscape_c, R.drawable.landscape_d, R.drawable.landscape_e, R.drawable.landscape_f, R.drawable.landscape_g,
+            R.drawable.landscape_h, R.drawable.landscape_i, R.drawable.landscape_j, R.drawable.landscape_k, R.drawable.landscape_l, R.drawable.landscape_m, R.drawable.landscape_n,
+            R.drawable.landscape_o, R.drawable.landscape_p, R.drawable.landscape_q, R.drawable.landscape_r, R.drawable.landscape_s, R.drawable.landscape_t};
+    private int[] bosses = new int[]{R.drawable.a_boss_a, R.drawable.a_boss_b, R.drawable.a_boss_c, R.drawable.a_boss_d, R.drawable.a_boss_e,R.drawable.a_boss_f,R.drawable.a_boss_g,R.drawable.a_boss_h,
+            R.drawable.a_boss_i,R.drawable.a_boss_j,R.drawable.a_boss_k};
+    private boolean BossonDraw = false;
+    private int Height = MainActivity.HEIGHT;
+    private int Width = MainActivity.WIDTH;
+    public int touches;
+
+    /**
+     * Задание волн
+     */
+    private int firstWave = 5;
+    private int secondWave = firstWave * 2;
+    private int thirdWave = secondWave * 2;
+    private int fourthWave = thirdWave * 2;
+    private int fifthWave = fourthWave * 2;
+    /**
+     * Конец задания волн
+     */
+    public static int countDeath = 0;
+    public static int countLive = 0;
+    private Random rnd = new Random();
+
+    public List<Bullet> ball = new LinkedList<>();
+    public Player player;
+    public List<Enemy> enemy = new LinkedList<>();
+    private Thread thread = new Thread(this);
+    public Land landing;
+    private Boss boss;
+
+    private MediaPlayer sound = new MediaPlayer();
+
+    /**
+     * Объект класса GameLoopThread
+     */
+
+    public GameManager gameLoopThread;
+    public int shotX;
+    public int shotY;
+    public int levelNumber = 1;
+
+
+    @Override
+    public void run() {
+        while (ok) {
+            if (!holder.getSurface().isValid()) {
+                continue;
+            }
+            try {
+                Thread.sleep(200);
+                setLevel(countLive);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public GameView(Context context) {
+        super(context);
+        holder = getHolder();
+        landing = new Land(this, BitmapFactory.decodeResource(getResources(), R.drawable.landscape_q));
+        boss = new Boss(this, setRandomBitmap(bosses), levelNumber);
+        gameLoopThread = new GameManager(this);
+        gameLoopThread.context = getContext();
+        LvlPackArray.add(imgsArrayLvl_b);
+        LvlPackArray.add(imgsArrayLvl_c);
+        LvlPackArray.add(imgsArrayLvl_d);
+        LvlPackArray.add(imgsArrayLvl_e);
+        LvlPackArray.add(imgsArrayLvl_f);
+        LvlPackArray.add(imgsArrayLvl_g);
+        LvlPackArray.add(imgsArrayLvl_h);
+        LvlPackArray.add(imgsArrayLvl_i);
+        LvlPackArray.add(imgsArrayLvl_j);
+        LvlPackArray.add(imgsArrayLvl_k);
+        player = new Player(this);
+        /**Рисуем все наши объекты и все все все*/
+        getHolder().addCallback(new SurfaceHolder.Callback() {
+            /** Создание области рисования */
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                gameLoopThread.setRunning(true);
+                if (gameLoopThread.getState() == Thread.State.NEW) {
+
+                    gameLoopThread.start();
+                }
+                if (thread.getState() == Thread.State.NEW) {
+                    thread.start();
+                } else {
+                    try {
+                        GameView.this.notify();
+                    } catch (Exception exx) {
+                        exx.printStackTrace();
+                    }
+                }
+            }
+
+            /** Изменение области рисования */
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            /*** Уничтожение области рисования */
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                boolean retry = true;
+                gameLoopThread.setRunning(false);
+                while (retry) {
+                    try {
+                        // ожидание завершение потока
+                        gameLoopThread.join();
+                        retry = false;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+    }
+
+    /**
+     * Функция рисующая все спрайты и фон
+     */
+    protected void onDraw(Canvas canvas) {
+        if (BossonDraw) {
+            boss.onDraw(canvas);
+            bossTest();
+        }
+    }
+
+    public Bullet createSprite(int resouce) {
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), resouce);
+        return new Bullet(this, bmp);
+    }
+
+    public boolean onTouchEvent(MotionEvent e) {
+        shotX = (int) e.getX();
+        shotY = (int) e.getY();
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                touches++;
+                ball.add(createSprite(R.drawable.bullet));
+            }
+            case MotionEvent.ACTION_MOVE: {
+                if (player.getPlLives() < 11) {
+                    ball.add(createSprite(R.drawable.rocket_wall));
+                }
+            }
+        }
+        return true;
+    }
+
+    /*Проверка на столкновения*/
+    public synchronized void testCollision() {
+        Iterator<Enemy> i = enemy.iterator();
+
+        while (i.hasNext()) {
+            Enemy enemies = i.next();
+            Iterator<Bullet> b = ball.iterator();
+            if (!enemies.isDeathFlag() && (Math.abs(player.x - enemies.x) <= (player.getWidth() / 2 + enemies.width))
+                    && (Math.abs(player.y - enemies.y) <= (player.getHeight() / 2 + enemies.height))) {
+                enemies.setDeathFlag(true);
+                enemies.onExplode();
+                i.remove();
+                enemies.bitmap.recycle();
+                player.setPlLives(player.getPlLives() - 1);
+                sound();
+            }
+            while (b.hasNext()) {
+                Bullet balls = b.next();
+                if (!enemies.isDeathFlag()) {
+                    if ((Math.abs(balls.x - enemies.x) <= (balls.width /*/ 2*/ + enemies.width))
+                            && (Math.abs(balls.y - enemies.y) <= (balls.height / 2 + enemies.height)) && (balls.y < Width - enemies.y)) {
+                        enemies.setDeathFlag(true);
+                        enemies.onExplode();
+                        b.remove();
+                        enemies.bitmap.recycle();
+                        countDeath++;
+                        sound();
+                    }
+                }
+            }
+
+        }
+    }
+
+    private void bossTest() {
+        Iterator<Bullet> b = ball.iterator();
+        if (!boss.isDeathFlag() && (Math.abs(player.x - boss.x) <= (player.getWidth() / 2 + boss.width))
+                && (Math.abs(player.y - boss.y) <= (player.getHeight() / 2 + boss.height))) {
+            boss.setReverse(true);
+            boss.countCrash--;
+            player.setPlLives(player.getPlLives() - 1);
+            sound();
+        }
+        while (b.hasNext()) {
+            Bullet balls = b.next();
+            if ((Math.abs(balls.x - boss.x) <= (balls.width / 2 + boss.width))
+                    && (Math.abs(balls.y - boss.y) <= (balls.height / 2 + boss.height))) {
+                boss.setReverse(true);
+                boss.countCrash--;
+                b.remove();
+                balls.bitmap.recycle();
+                countDeath++;
+                sound();
+            }
+        }
+    }
+
+    /*Звук на выстрел*/
+    private void sound() {
+        try {
+            sound.stop();
+            sound.reset();
+            AssetFileDescriptor descriptor;
+            descriptor = getContext().getAssets().openFd("blast.mp3");
+            sound.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+            descriptor.close();
+            sound.prepare();
+            sound.start();
+        } catch (IllegalStateException illexc) {
+            illexc.printStackTrace();
+        } catch (IOException illexc) {
+            illexc.printStackTrace();
+        }
+    }
+
+    private synchronized void setLevel(int cLive) {
+        countLive++;
+        int index;
+
+        if (cLive >= 0 && cLive <= firstWave) {
+            index = rnd.nextInt(tempImgsArray.length - 4);
+            enemy.add(new Enemy(this, BitmapFactory.decodeResource(getResources(), tempImgsArray[index]), levelNumber + 1));
+        }
+        if (cLive > firstWave && cLive <= secondWave) {
+            index = rnd.nextInt(tempImgsArray.length - 3);
+            enemy.add(new Enemy(this, BitmapFactory.decodeResource(getResources(), tempImgsArray[index]), levelNumber + 2));
+        }
+        if (cLive > secondWave && cLive <= thirdWave) {
+            index = rnd.nextInt(tempImgsArray.length - 2);
+            enemy.add(new Enemy(this, BitmapFactory.decodeResource(getResources(), tempImgsArray[index]), levelNumber + 3));
+        }
+        if (cLive > thirdWave && cLive <= fourthWave) {
+            index = rnd.nextInt(tempImgsArray.length - 1);
+            enemy.add(new Enemy(this, BitmapFactory.decodeResource(getResources(), tempImgsArray[index]), levelNumber + 4));
+        }
+        if (cLive > fourthWave && cLive <= fifthWave) {
+            index = rnd.nextInt(tempImgsArray.length);
+            enemy.add(new Enemy(this, BitmapFactory.decodeResource(getResources(), tempImgsArray[index]), levelNumber + 5));
+        }
+        if (cLive > fifthWave) {
+
+            if (boss.countCrash > 0) {
+                BossonDraw = true;
+                enemy.add(new Enemy(this, BitmapFactory.decodeResource(getResources(), R.drawable.rocket), 15, boss.x, boss.y));
+            } else
+            if (boss.countCrash <= 0 && boss.x <= 0) {
+                if (player.getPlLives() < 10 && player.getPlLives() > 0) {
+                    player.setPlLives(player.getPlLives() + 1);
+                }
+                BossonDraw = false;
+                levelNumber++;
+                Bullet.mSpeed--;
+                countLive = 0;
+                setLevel(countLive);
+                //чистим ресурсы
+                lvlRecycle();
+                landing = new Land(this, setRandomBitmap(landscapes));
+                boss = new Boss(this, setRandomBitmap(bosses), levelNumber);
+            }
+        }
+
+    }
+
+    public Bitmap setRandomBitmap(int[] imgsArray) {
+        return BitmapFactory.decodeResource(getResources(), imgsArray[rnd.nextInt(imgsArray.length - 1)]);
+    }
+
+    public synchronized void lvlRecycle() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                tempImgsArray = LvlPackArray.get(rnd.nextInt(LvlPackArray.size() - 1));
+                ball = null;
+                ball = new LinkedList<>();
+                enemy = null;
+                enemy = new LinkedList<>();
+            }
+        }).start();
+    }
+
+    public void pause() {
+        ok = false;
+        while (true) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            break;
+        }
+        thread = null;
+    }
+
+    public void resume() {
+        ok = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+}
